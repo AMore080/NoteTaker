425149168f0b3fa6e1281afd25a6ecbc3e9ba4da
Alex <alexmore080@gmail.com>
2022-09-21 13:20:19 -0500

    server backend established

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.watchFile);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-const apiRouter = require('./routes/apiRouter')
-app.use(apiRouter);
-const htmlRouter = require('./routes/htmlRouter');
-app.use(htmlRouter);
+app.get('/api/notes', (req,res) => {
+    readFileAsync('./db/db.json','utf-8').then(function(data){
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+
+app.post('/api/notes', (req,res) => {
+    const note = req.body;
+     readFileAsync('./db/db.json',"utf-8").then(function(data){
+        const notes = [].concat(JSON.parse(data));
+        note.id = uuidv4();
+        notes.push(note);
+        return notes;
+    }).then((notes) => {
+        writeFileAsync('./db/db.json',JSON.stringify(notes))
+        res.json(note);
+    })
+})
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
